e00b065b6105ddb9f20dbfb76b6e7afa1d659cd6
Mohammed Ali <x.c0d3rz000@gmail.com>
2017-06-14 01:28:12 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 var answer = require('./index');
-new answer({q:'who is the owner of google', limit:2},function(res){
-   for(var r in res ){
-       console.log(res[r].answer);
-   }
-});
+var options = {
+    q: "Who is the owner of google", // question
+    limit: 2 // results limit
+};
+new answer(options, function (res) {
+    for (var r in res) {
+        console.log(res[r].answer);
+    }
+})
+    .error = function (err) {
+        console.error(err);
+    };
+// => Larry Page and Sergey Brin, who were Ph.D. students at Stanford \n Google Chrome is owned by the founder of Google and the Google company. It is not just owned by a single person.
